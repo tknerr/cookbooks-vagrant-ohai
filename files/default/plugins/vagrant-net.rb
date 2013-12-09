15ee2806fcec295aafc84063e106772f57459ef2
Seva Polyakov <ctrlok@gmail.com>
2013-12-09 19:18:48 +0200

    will work with new chef

--- files/default/plugins/vagrant-net.rb
+++ files/default/plugins/vagrant-net.rb
@@ -1,7 +1,7 @@
 
 # borrowed from https://gist.github.com/2050259
 
-provide "ipaddress"
+provides "ipaddress"
 require_plugin "#{os}::network"
 network["interfaces"]["eth1"]["addresses"].each do |ip, params|
   if params['family'] == ('inet')
