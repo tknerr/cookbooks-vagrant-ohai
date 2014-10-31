fab597b5ebf9746630d83f57b89629490007412d
TonyCerrit <Anthony.Cerritelli@espn.com>
2014-10-31 11:23:20 -0400

    update for use with berkshelf

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "cookbooks-vagrant-ohai"
 license          "Apache 2.0"
 description      "Distributes ohai plugins for vagrant chef nodes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
